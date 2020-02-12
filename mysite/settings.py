253fc27605d1e755fcf3b6c71f25b021534629ec
camila-moscibrocki <camilamoscibrocki@gmail.com>
2020-02-12 19:03:56 -0300

    Adicionado views e tamplte para os objetos posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '**uwi@%lzp65lhnuo1%-uc4ffg3dk!#a6#wg*^td8=3q&6s6o4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
